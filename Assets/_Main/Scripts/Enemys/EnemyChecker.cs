f8ee4a15d9aef73047dd2ea0e5664b4af8d64919
David Echeverri <david.echeverri@timba.co>
2021-08-07 18:13:59 -0500

    add enemy states

---
+++ Assets/_Main/Scripts/Enemys/EnemyChecker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyChecker : MonoBehaviour
+{
+    public Action<bool> isplayerInRange;
+    public GameObject playerRef;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")){
+            playerRef = other.gameObject;
+            isplayerInRange?.Invoke(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.CompareTag("Player")){
+            isplayerInRange?.Invoke(false);
+        }
+    }
+}
