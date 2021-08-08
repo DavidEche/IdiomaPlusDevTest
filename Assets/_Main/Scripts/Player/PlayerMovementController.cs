6b6d62daa4f06f82718f3580513423ef6852e666
David Echeverri <david.echeverri@timba.co>
2021-08-07 21:15:31 -0500

    update move Character

--- Assets/_Main/Scripts/Player/PlayerMovementController.cs
+++ Assets/_Main/Scripts/Player/PlayerMovementController.cs
@@ -1,31 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerMovementController : MonoBehaviour
 {
-    [SerializeField] private CharacterController _controller;
-    [SerializeField] private float speed = 6f;
-    [SerializeField] private float turnSmoothTime = 0.1f;
-    [SerializeField] private float turnSmoothVelocity = 0.1f;
-    [SerializeField] private Animator _animator;
+    [SerializeField] private InputActionReference movementControl;
+    [SerializeField] private InputActionReference jumpController;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float rotationSpeed = 4f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        PlayerMove();
+    [SerializeField] private Transform cameraMainTranform;
+    [SerializeField] private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    private void OnEnable() {
+        movementControl.action.Enable();
+        jumpController.action.Enable();
     }
 
-    private void PlayerMove(){
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+    private void OnDisable() {        
+        movementControl.action.Disable();
+        jumpController.action.Disable();
+    }
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        Vector2 movement = movementControl.action.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        move = cameraMainTranform.forward * move.z + cameraMainTranform.right * move.x;
+        move.y = 0f;
+        controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if(direction.magnitude >= 0.1f){
-            float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle, ref turnSmoothVelocity,turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle,0f);
-            _controller.Move(direction* speed * Time.deltaTime);
+
+
+        // Changes the height position of the player..
+        if (jumpController.action.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if(movement != Vector2.zero){
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTranform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation,Time.deltaTime * rotationSpeed);
         }
     }
 }
