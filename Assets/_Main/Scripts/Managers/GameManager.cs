365734e0cddd2e4f0c114748db8494aef80b3190
David Echeverri <david.echeverri@timba.co>
2021-08-08 21:12:14 -0500

    added loadScenes

--- Assets/_Main/Scripts/Managers/GameManager.cs
+++ Assets/_Main/Scripts/Managers/GameManager.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : ManagerBase
 {
     [SerializeField] private Transform defaultPlayerPosition;
     [SerializeField] private PlayerStats player;
     [SerializeField] private Stats defaultStats;
+    [SerializeField] private string sceneName;
 
 
     private void Start() {
         player.InitializeStats(defaultStats);
         player.gameObject.transform.position = defaultPlayerPosition.position;
+    }
+
+    public void BackToLobby(){
+        SceneLoader.LoadLevel(sceneName);
     }
 }
---
+++ Assets/_Main/Scripts/Services/UndestroyableSingleton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class UndestroyableSingleton : MonoBehaviour
+{
+	private static UndestroyableSingleton _instance;
+
+	private void Awake() {
+		if (_instance == null) {
+			_instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		} else if (_instance != this) {
+			Destroy(this.gameObject);
+		}
+	}
+}
